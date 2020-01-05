76e82cbff04c7539854cd213cd4b590139836a75
TV321 <tvlahovic7@gmail.com>
2020-01-05 23:54:54 +0100

    navigation layout

--- src/Components/Navigation/Navigation.js
+++ src/Components/Navigation/Navigation.js
@@ -1,24 +1,33 @@
 import React from 'react';
 import './Navigation.sass';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFacebookSquare } from '@fortawesome/free-brands-svg-icons'
+import { faInstagram } from '@fortawesome/free-brands-svg-icons'
+import { faYoutube} from '@fortawesome/free-brands-svg-icons'
+
+const facebook = <FontAwesomeIcon icon={ faFacebookSquare } />
+const instagram = <FontAwesomeIcon icon={ faInstagram } />
+const youtube = <FontAwesomeIcon icon={ faYoutube } />
+
 const Navigation = () =>{
     return(
-        <nav>
+        <nav className="container">
             <div id="logo-nav">
                 <p>Evil Corp.</p>
             </div>
             <div id="main-nav">
                 <ul>
+                    <li>Home</li>
                     <li>About</li>
-                    <li>About</li>
-                    <li>About</li>
+                    <li>Contact</li>
                 </ul>
             </div>
             <div id="social-nav">
                 <ul>
-                    <li>Face</li>
-                    <li>Insta</li>
-                    <li>Youtube</li>
+                    <li>{ facebook }</li>
+                    <li>{ instagram }</li>
+                    <li>{ youtube }</li>
                 </ul>
             </div>
         </nav>
